21adcc37a1bfb04e7a66b282e5936b059268cbcf
MathieuLafontaine <mathieu.lafontaine@hotmail.fr>
2025-06-18 15:07:59 -0400

    les fichier de l'examen

---
+++ examen3/src/main.js
@@ -0,0 +1,12 @@
+import { greet } from "./assets/js/utilis.js";
+
+greet("Lilia Ould Hocine");
+
+const liensHTML = document.querySelectorAll(".nav-header-link");
+
+//Debut de la navigaiton sans chargement
+/* liensHTML.forEach(lien => {
+  lien.addEventListener("click", intermediaire => {
+    intermediaire.preventDefault();
+  });
+}); */
